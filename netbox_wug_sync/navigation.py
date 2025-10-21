aa5754dd83833cd7407e2e1300761cd6c1e38815
bassslap <phillip@progress.com>
2025-10-21 12:58:48 +0000

    remove icon in navigation.py

--- netbox_wug_sync/navigation.py
+++ netbox_wug_sync/navigation.py
@@ -9,8 +9,7 @@ from netbox.plugins import PluginMenuButton, PluginMenuItem
 # Define menu buttons
 add_connection_button = PluginMenuButton(
     link='plugins:netbox_wug_sync:wugconnection_add',
-    title='Add Connection',
-    icon_class='mdi mdi-plus-thick'
+    title='Add Connection'
 )
 
 # Define menu items
@@ -18,22 +17,18 @@ menu_items = (
     PluginMenuItem(
         link='plugins:netbox_wug_sync:dashboard',
         link_text='Dashboard',
-        icon_class='mdi mdi-view-dashboard'
     ),
     PluginMenuItem(
         link='plugins:netbox_wug_sync:wugconnection_list',
         link_text='Connections',
-        icon_class='mdi mdi-server-network',
         buttons=[add_connection_button]
     ),
     PluginMenuItem(
         link='plugins:netbox_wug_sync:wugdevice_list',
         link_text='Devices',
-        icon_class='mdi mdi-devices'
     ),
     PluginMenuItem(
         link='plugins:netbox_wug_sync:wugsynclog_list',
         link_text='Sync Logs',
-        icon_class='mdi mdi-clipboard-text'
     ),
 )
